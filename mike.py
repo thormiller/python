06bdd1ef19bda8e47857695dbcf8e3095b5701ad
thormiller <54950950+thormiller@users.noreply.github.com>
2019-11-12 09:45:49 -0800

    Update mike.py

--- mike.py
+++ mike.py
@@ -1,6 +1,6 @@
 import turtle
 t = turtle.Turtle()
-
+t.speed(1)
   
   
 def start(t,length):
@@ -13,35 +13,53 @@ def octagon(t,length):
 		t.forward(length)
 		t.right(45)
 		
+if t.begin_fill():
+   t.fillcolor("red")
+ 
+else:
+	t.color("green") 
+ 
+ 
+for i in range(75):
+    t.speed(0)	  
+    t.goto(0,-150)
+    t.circle(50)
+    t.right(5)    
 
-
+ 
 def callMike():
     w = turtle.Screen()
     w.setup(1000,1000)
     w.clear()
     w.bgcolor("white")
-    t = turtle.Turtle()
-    t.penup()
+    t.pendown()
     x = 50
     y = 90
+    t.goto(x,y)
+    t.pendown()
     count = 0             
     t.color("blue")
+
     while (count < 10):
         t.goto(x,y)
         t.pendown()
-        t.circle(100)
+        t.fillcolor("green")
+        t.circle(100)              
         start(t,100)
         octagon(t,100)
         count = count + 1
         x = x - 10 
         y = y + 10
-        if (count > 1):
-            t.color("red",)
-     		 
+        if (count > 5):
+            t.color("red",)                    	                            	 
+        print(x,y,count)
+        
+        if (count < 5):
+            t.color("green")
         print(x,y,count)
     w.exitonclick()
 		
-
+t.end_fill()
 
 
 def main():
@@ -49,3 +67,4 @@ def main():
     
 if __name__ == '__main__':
 	main()
+
