ca84c42fd6eb6b72aacb99b3dfffdb3a568ea70b
thormiller <54950950+thormiller@users.noreply.github.com>
2019-11-20 10:07:55 -0800

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,24 @@
+shane = input("user: ")
+if (shane==("shane")):
+   print("Aceess!!")   
+   shane = input("password: ")
+   if (shane==("zyl7913")):
+      print("Aceess!!")
+      cap =input("Personal file continue y/n: ") 
+      if (cap ==("yes") or cap ==("y") ):
+        print("-----Name: Shane")
+        print("------Last: Miller")
+        print("-------Born: China")
+        print("---------")
+        print("----------")
+      elif(not cap ==("yes") or cap ==("y")):
+          print("Exit")
+   elif(not shane ==("shane")):
+	   print("Aceess fail!!")
+      
+                            		        
+   
+
+else:
+	print("Aceess fail!!")
+
